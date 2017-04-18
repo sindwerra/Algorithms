56f1cd284f0493f34b727dc33e72b1eee5e06763
sindwerra <sindwerra@hotmail.com>
2017-04-18 10:22:33 -0700

    Correct for sorting of Linked List

--- leetcode/Linked List/#148-Sort List/main.py
+++ leetcode/Linked List/#148-Sort List/main.py
@@ -43,3 +43,71 @@ class Solution(object):
                 temp = s[0]
 
         return sorted_x[0][0]
+
+'''
+刚开始做的方法不符合题目要求，这里用的归并排序做
+Beat 85.63%
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        
+        slow, fast = head, head.next
+
+        if not fast:
+            return head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        left = head
+        right = slow.next
+        slow.next = None
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        if not left:
+            return right
+        if not right:
+            return left
+        
+        dummy = ListNode('dummy')
+        cur = dummy
+
+        while left and right:
+            if left.val < right.val:
+                cur.next = left
+                left = left.next
+            else:
+                cur.next = right
+                right = right.next
+            cur = cur.next
+        
+        while left:
+            cur.next = left
+            left = left.next
+            cur = cur.next
+        
+        while right:
+            cur.next = right
+            right = right.next
+            cur = cur.next
+        
+        return dummy.next
